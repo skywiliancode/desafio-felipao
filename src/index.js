4cd2d6ca4f62b2811a7b400c6e153c86f0eba972
skywilian <codewilian852@gmail.com>
2023-12-14 11:19:19 -0300

    Projeto desafio felipao finalizado

--- src/index.js
+++ src/index.js
@@ -1,2 +1,26 @@
-console.log('Projeto do felipão')
-console.log('Oi')
+// Estrutura de entrada (input)
+let nome = 'João'
+let xp = 10001
+let nivel = 'ferro'
+
+// Verificações (process)
+if(xp < 1000){
+  nivel = 'ferro'
+}else if((xp > 1000) && (xp <= 2000)){
+  nivel = 'Bronze'
+}else if((xp > 2000) && (xp <= 5000)){
+  nivel = 'Prata'
+}else if((xp > 6000) && (xp <= 7000)){
+  nivel = 'Ouro'
+}else if((xp > 7000) && (xp <= 8000)){
+  nivel = 'Platina'
+}else if((xp > 8000) && (xp <= 9000)){
+  nivel = 'Ascendente'
+}else if((xp > 9000) && (xp <= 10000)){
+  nivel = 'Imortal'
+}else{
+  nivel = 'Radiante'
+}
+
+// Estrutura de saída (output)
+console.log(`O Herói de nome ${nome} está no nível de ${nivel}`)
